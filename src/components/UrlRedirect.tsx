6e9d453f706c60b1ced7c0252ac5eac7f04e6afa
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-23 10:28:38 -0700

    redirect to telegram group

--- src/components/UrlRedirect.tsx
+++ src/components/UrlRedirect.tsx
@@ -1,46 +1,11 @@
-import _ from 'lodash';
 import React, { useEffect } from 'react';
 
-import { APIHelper } from '../helpers/APIHelper';
 import { GenericHelper } from '../helpers/GenericHelper';
-import { ICampaign } from '../types/payer.types';
 
 export const UrlRedirect: React.FC = () => {
-  const promoterId = GenericHelper.getUrlQueryParamByName("promoterId");
-  // const payerId = GenericHelper.getUrlQueryParamByName("payerId");
-  // const groupUrl = "https://t.me/brothersvagasp";
-
-  // randomize
-
-  const payers: ICampaign[] = [
-    { payerId: 5, groupUrl: "https://t.me/empregourgenteSPc" },
-    { payerId: 3, groupUrl: "https://t.me/brothersvagasp" },
-  ];
-  let campaign: ICampaign | null = null;
-
-  const n = _.random(10);
-
-  if (n >= 0 && n <= 4) {
-    campaign = payers[0];
-  }
-  if (n >= 5 && n <= 10) {
-    campaign = payers[1];
-  }
-
   useEffect(() => {
-    if (campaign) {
-      (async () => {
-        const response = await APIHelper.request("POST", "/credit", {
-          promoterId,
-          payerId: campaign.payerId,
-        });
-
-        console.log(response.data);
-
-        GenericHelper.crossBrowserUrlRedirect(campaign.groupUrl);
-      })();
-    }
-  }, [campaign, promoterId]);
+    GenericHelper.crossBrowserUrlRedirect("https://t.me/empregourgenteSPc");
+  }, []);
 
   return <p>Aguarde enquanto redirecionamos...</p>;
 };
